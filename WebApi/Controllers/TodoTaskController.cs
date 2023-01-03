b0619325f85674eba640a776dec14d55a6466456
yaoshuanggui <1070342164.com>
2023-01-04 02:32:32 +0800

    update

---
+++ WebApi/Controllers/TodoTaskController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    /// <summary>
+    /// TodoTask
+    /// </summary>
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class TodoTaskController : ControllerBase
+    {
+        /// <summary>
+        /// Get
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// Get
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// Post
+        /// </summary>
+        /// <param name="value"></param>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        /// <summary>
+        /// Put
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        /// <summary>
+        /// Delete
+        /// </summary>
+        /// <param name="id"></param>
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,32 +1,60 @@
+using Microsoft.OpenApi.Models;
+using System.IO;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//builder.Services.AddMvc();
+
+builder.Services.AddEndpointsApiExplorer();
+
+// 添加Swagger服务
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo 
+    { 
+        Title = "ToDoList API",
+        Version = "V1",
+        Description = ".NET7使用MongoDB开发ToDoList系统",
+        Contact = new OpenApiContact
+        {
+            Name = "GitHub源码地址",
+            Url = new Uri("https://github.com/YSGStudyHards/YyFlight.ToDoList")
+        }
+    });
+
+    //// 获取xml文件名
+    //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    //// 获取xml文件路径
+    //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    //// 添加控制器层注释，true表示显示控制器注释
+    //options.IncludeXmlComments(xmlPath, true);
+    ////c.IncludeXmlComments(path, true); // true : 显示控制器层注释
+    //options.OrderActionsBy(o => o.RelativePath); // 对action的名称进行排序，如果有多个，就可以看见效果了。
+
+    // using System.Reflection;
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+// 添加Swagger相关中间件
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
 });
 
-app.Run();
+app.UseStaticFiles();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
+app.Run();
