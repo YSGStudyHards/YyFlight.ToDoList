08eead6c6fa089480b4b3743e59d83c34d623f92
Edwin <1070342164@qq.com>
2023-06-29 23:05:35 +0800

    add 前端框架搭建

---
+++ YyToDoBlazor/Models/LoginParams.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YyToDoBlazor.Models
+{
+    public class LoginParams
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
